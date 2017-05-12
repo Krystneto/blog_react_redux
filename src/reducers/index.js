1bb8e3ae68aafe40ed97e1dd5f6c236d767a0fc9
Kristopher Parrillo <kkparrillo@gmail.com>
2017-05-11 19:17:47 -0700

    adds new posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './Reducer_Posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
